e742acedba2b9b79b85d6754d7e13b5da1bf613a
eniallator <eniallatormc@gmail.com>
2017-09-11 08:01:39 +0100

    Added a source_code command

--- Commands.py
+++ Commands.py
@@ -8,3 +8,12 @@ COMMANDS.append({
     'help': 'Replies with "Pong!"',
     'func': _ping
 })
+
+def _source_code(client, message, user_command):
+    return client.send_message(message.channel, 'https://github.com/eniallator/Discord-EniBot')
+
+COMMANDS.append({
+    'start': 'source_code',
+    'help': 'Replies with the source code link',
+    'func': _source_code
+})
