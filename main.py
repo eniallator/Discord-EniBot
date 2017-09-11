479afbc2445bd8902431fdeda450f0cede551b67
eniallator <eniallatormc@gmail.com>
2017-09-11 08:18:52 +0100

    Refactored on_message a bit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from time import gmtime, strftime
+
 import asyncio
 import discord
-
-from time import gmtime, strftime
 
 import Credentials
 from Commands import COMMANDS
@@ -16,6 +16,22 @@ def _log(msg_to_log):
     print(_get_time() + msg_to_log)
     # Might do some file I/O stuff here
 
+def _help_response(message, user_command):
+    _log(str(message.author) + ' ran: "' + user_command + '" in server: ' + message.server.name)
+    response = 'Help displayed in the following format:\n"Command": Help_for_command\n'
+    for command in COMMANDS:
+        response += '\n"' + command['start'] + '": ' + command['help']
+    return CLIENT.send_message(message.channel, response)
+
+def _command_handler(message, user_command):
+    for command in COMMANDS:
+        if user_command.lower().split(' ')[0] == command['start']:
+            _log(str(message.author) + ' ran: "' + user_command + '" in server: ' + message.server.name)
+            return command['func'](CLIENT, message, user_command)
+    else:
+        _log(str(message.author) + ' tried to run: "' + user_command + '" in server: ' + message.server.name)
+        return CLIENT.send_message(message.channel, 'Unknown command. Use "help" to get a list of commands.')
+
 @CLIENT.event
 async def on_ready():
     print('Logged in as')
@@ -29,20 +45,8 @@ async def on_message(message):
     if str(message.channel) == 'bot_testing' and not message.author.bot and message.content.startswith(prefix):
         user_command = message.content.replace(prefix, '', 1)
         if user_command.lower().split(' ')[0] == 'help':
-            _log(str(message.author) + ' ran: "' + user_command + '" in server: ' + message.server.name)
-            response = 'Help displayed in the following format:\n"Command": Help_for_command\n'
-            for command in COMMANDS:
-                response += '\n"' + command['start'] + '": ' + command['help']
-
-            await CLIENT.send_message(message.channel, response)
+            await _help_response(message, user_command)
         else:
-            for command in COMMANDS:
-                if user_command.lower().split(' ')[0] == command['start']:
-                    _log(str(message.author) + ' ran: "' + user_command + '" in server: ' + message.server.name)
-                    await command['func'](CLIENT, message, user_command)
-                    break
-            else:
-                _log(str(message.author) + ' tried to run: "' + user_command + '" in server: ' + message.server.name)
-                await CLIENT.send_message(message.channel, 'Unknown command. Use "help" to get a list of commands.')
+            await _command_handler(message, user_command)
 
 CLIENT.run(Credentials.TOKEN)
