c6c77e85db87992b183621515314f22498641d40
eniallator <eniallatormc@gmail.com>
2018-09-09 21:12:13 +0100

    Added a get_status command to the minecraft command system

--- src/CommandSystems/Minecraft.py
+++ src/CommandSystems/Minecraft.py
@@ -1,7 +1,10 @@
 """Commands todo with minecraft"""
 import re
 import random
+from socket import timeout
+from discord import Embed
 from src.CommandSystem import CommandSystem
+from src.minecraftStatusPing import StatusPing
 
 MINECRAFT_COMMANDS = CommandSystem(help_summary='Commands todo with minecraft.')
 
@@ -24,4 +27,59 @@ MINECRAFT_COMMANDS.add_command(
     'ran_colours',
     cmd_func=_ran_colours,
     help_summary='Inserts random colours for each character.'
+)
+
+def ping(host, port):
+    try:
+        port = int(port)
+        if port >= 0 and port < 2**16:
+            status_ping = StatusPing(host, port)
+            return status_ping.get_status()
+        else:
+            return 'Port out of range.'
+    except ValueError:
+        return 'Port is not a number.'
+
+async def _get_status(client, user_command, message):
+    args = user_command.split(' ')[2:]
+
+    if not args:
+        await client.send_message(message.channel, 'Need a server to connect to.')
+        return
+
+    if ':' in args[0]:
+        args = args[0].split(':')
+
+    host = args[0]
+    port = args[1] if len(args) > 1 else '25565'
+
+    try:
+        response = ping(host, port)
+    except timeout:
+        await client.send_message(message.channel, 'Connection timed out.')
+        return
+    except Exception:
+        await client.send_message(message.channel, 'Something went wrong...')
+        return
+
+    if isinstance(response, dict):
+        players_online = [player['name'] for player in (response['players']['sample'] if 'sample' in response['players'] else [])]
+        await client.send_message(message.channel, embed=Embed(
+            title='**' + host + ':' + port + '**',
+            type='rich',
+            colour=0x32ff32,
+            description=
+            '***Description***: ' + response['description']['text'] +
+            '\n\n***Players online***: ' + str(response['players']['online']) + '/' + str(response['players']['max']) +
+            '\n\n***Player list***:\n' + '\n'.join(players_online)
+        ))
+    elif isinstance(response, str):
+        await client.send_message(message.channel, response)
+    else:
+        await client.send_message(message.channel, 'Connection timed out.')
+
+MINECRAFT_COMMANDS.add_command(
+    'get_status',
+    cmd_func=_get_status,
+    help_summary='Get the status of any minecraft server. Args: [server IP] [port]'
 )
