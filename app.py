934fd0b5cc89ccd157a4496c041c46debc773d50
eniallator <eniallatormc@gmail.com>
2018-11-11 21:11:51 +0000

    Added a multi command system to the bot for running multiple commands at once

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from Config import OWNER
 from src.Commands import COMMANDS
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
+MULTI_COMMAND_LANGUAGE = 'enibot'
 
 if not TOKEN:
     try:
@@ -48,17 +49,8 @@ async def on_ready():
             LOG_USER['member'] = member
     await _log('Bot logged in with name: "' + CLIENT.user.name + '" and id: ' + CLIENT.user.id + '\n', first_log=True)
 
-@CLIENT.event
-async def on_message(message):
-    """Handles any user commands"""
-    prefix = '<@' + CLIENT.user.id + '> '
-    user_command = ''
-    if message.content.startswith(prefix):
-        user_command = message.content.replace(prefix, '', 1)
-    elif not message.server:
-        user_command = message.content
-
-    if CLIENT.user.id != message.author.id and user_command:
+async def command_hander(message, user_command, logging=True):
+    if logging:
         log_message = str(message.author) + ' ran: "' + user_command + '"'
         if message.server:
             log_message += ' in server: ' + message.server.name
@@ -68,14 +60,45 @@ async def on_message(message):
         if LOG_USER['name'] != str(message.author) or message.server:
             await log_routine
 
-        if user_command.lower().split(' ')[0] == 'help':
-            help_command = ' '.join(user_command.split(' ')[1:])
-            help_message = COMMANDS.get_help(help_command, CLIENT, help_command, message)
-            await CLIENT.send_message(message.channel, help_message)
-        else:
-            output = await COMMANDS.execute(user_command, CLIENT, user_command, message)
-            if isinstance(output, str):
-                await CLIENT.send_message(message.channel, output)
+    if user_command.lower().split(' ')[0] == 'help':
+        help_command = ' '.join(user_command.split(' ')[1:])
+        help_message = COMMANDS.get_help(help_command, CLIENT, help_command, message)
+        await CLIENT.send_message(message.channel, help_message)
+    else:
+        output = await COMMANDS.execute(user_command, CLIENT, user_command, message)
+        if isinstance(output, str):
+            await CLIENT.send_message(message.channel, output)
+
+async def multi_command_handler(message, multi_command):
+    cmds = multi_command.split('\n')
+    cmds = list(filter(lambda cmd: cmd != '', cmds))
+    max_cmds = 10
+    if len(cmds) > max_cmds:
+        await CLIENT.send_message(message.channel, 'Error: multi command handler can handle at max ' + str(max_cmds) + ' commands')
+        _log(str(message.author) + ' tried to run ' + str(len(cmds)) + ' commands within a multi-command')
+        return
+
+    log_message = str(message.author) + ' ran a multi-command containing: "' + '", "'.join(cmds) + '"'
+    suffix = ' in server: "' + message.server.name + '"' if message.server else ' in a private message'
+    _log(log_message + suffix)
+    for cmd in cmds:
+        await command_hander(message, cmd, logging=False)
+
+@CLIENT.event
+async def on_message(message):
+    """Handles any user commands"""
+    prefix = '<@' + CLIENT.user.id + '> '
+    multi_command_prefix = '```' + MULTI_COMMAND_LANGUAGE + '\n'
+    multi_command_start = message.content.find(multi_command_prefix)
+    if message.content.startswith(prefix):
+        user_command = message.content.replace(prefix, '', 1)
+        await command_hander(message, user_command)
+    elif multi_command_start >= 0:
+        multi_command = message.content[multi_command_start + len(multi_command_prefix):]
+        multi_command = multi_command[:multi_command.find('```')]
+        await multi_command_handler(message, multi_command)
+    elif not message.server:
+        user_command = message.content
 
 
 CLIENT.run(TOKEN)
