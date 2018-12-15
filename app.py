0a59bf43917bce6806d60441ac8ecd1373da8bec
eniallator <eniallatormc@gmail.com>
2018-12-15 19:07:45 +0000

    Extracted logging into it's own file

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 import sys
 import os
 import configparser
-from time import gmtime, strftime
 import discord
 from Config import OWNER, MULTI_COMMAND_LANGUAGE
+from src.Logger import Logger
 from src.Commands import COMMANDS
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
@@ -23,62 +23,25 @@ if not TOKEN:
 
 CLIENT = discord.Client()
 
-
-LOG_USER = {}
-LOG_USER['name'] = OWNER
-
-def _get_time():
-    raw_time = strftime("%Y/%m/%d %H:%M:%S", gmtime())
-    return '[' + raw_time + '] '
-
-async def get_last_msg():
-    if 'member' not in LOG_USER:
-        return
-
-    await CLIENT.start_private_message(LOG_USER['member'])
-    for channel in CLIENT.private_channels:
-        if len(channel.recipients) == 1 and channel.recipients[0].id == LOG_USER['member'].id:
-            log_channel = channel
-
-    async for msg in CLIENT.logs_from(log_channel, limit=1):
-        return msg
-
-async def _log(msg_to_log, first_log=False):
-    timestamp_msg = _get_time() + msg_to_log
-    edit_last_msg = False
-
-    if first_log:
-        last_msg = await get_last_msg()
-        if last_msg and last_msg.author.id == CLIENT.user.id and last_msg.content.startswith('-'):
-            edit_last_msg = True
-        else:
-            timestamp_msg = '-' * 90 + '\n' + timestamp_msg
-
-    print(timestamp_msg)
-    if edit_last_msg:
-        await CLIENT.edit_message(last_msg, last_msg.content + '\r\n' + timestamp_msg)
-    elif 'member' in LOG_USER:
-        await CLIENT.send_message(LOG_USER['member'], timestamp_msg)
-
+logger = Logger(CLIENT, OWNER)
 
 @CLIENT.event
 async def on_ready():
     """When the bot logs in to discord"""
     for member in CLIENT.get_all_members():
-        if str(member) == LOG_USER['name']:
-            LOG_USER['member'] = member
-    await _log('Bot logged in with name: "' + CLIENT.user.name + '" and id: ' + CLIENT.user.id + '\n', first_log=True)
+        if str(member) == logger.get_dest_name():
+            logger.set_member(member)
+            break
+    await logger.log('Bot logged in with name: "' + CLIENT.user.name + '" and id: ' + CLIENT.user.id + '\n', first_log=True)
 
 async def command_hander(message, user_command, logging=True):
-    if logging:
+    if logging and logger.get_dest_name() != str(message.author) or message.server:
         log_message = str(message.author) + ' ran: "' + user_command + '"'
         if message.server:
             log_message += ' in server: ' + message.server.name
         else:
             log_message += ' in a private message'
-        log_routine = _log(log_message)
-        if LOG_USER['name'] != str(message.author) or message.server:
-            await log_routine
+        await logger.log(log_message)
 
     if user_command.lower().split(' ')[0] == 'help':
         help_command = ' '.join(user_command.split(' ')[1:])
@@ -92,19 +55,17 @@ async def command_hander(message, user_command, logging=True):
 async def multi_command_handler(message, multi_command):
     cmds = multi_command.split('\n')
     cmds = list(filter(lambda cmd: cmd != '', cmds))
-    log_routine = None
     max_cmds = 10
     if len(cmds) > max_cmds:
         await CLIENT.send_message(message.channel, 'Error: multi command handler can handle at max ' + str(max_cmds) + ' commands')
-        log_routine = _log(str(message.author) + ' tried to run ' + str(len(cmds)) + ' commands within a multi-command')
+        await logger.log(str(message.author) + ' tried to run ' + str(len(cmds)) + ' commands within a multi-command')
         return
 
     log_message = str(message.author) + ' ran a multi-command containing: "' + '", "'.join(cmds) + '"'
     suffix = ' in server: "' + message.server.name + '"' if message.server else ' in a private message'
-    log_routine = _log(log_message + suffix)
 
-    if LOG_USER['name'] != str(message.author) or message.server:
-        await log_routine
+    if logger.get_dest_name() != str(message.author) or message.server:
+        await logger.log(log_message + suffix)
 
     for cmd in cmds:
         await command_hander(message, cmd, logging=False)
---
+++ src/Logger.py
@@ -0,0 +1,45 @@
+from time import gmtime, strftime
+
+class Logger:
+    def __init__(self, client, log_dest):
+        self._client = client
+        self._log_user = {'name': log_dest}
+
+    def get_dest_name(self):
+        return self._log_user['name']
+
+    def set_member(self, member):
+        self._log_user['member'] = member
+
+    def _get_time(self):
+        raw_time = strftime("%Y/%m/%d %H:%M:%S", gmtime())
+        return '[' + raw_time + '] '
+
+    async def _get_last_msg(self):
+        if 'member' not in self._log_user:
+            return
+
+        await self._client.start_private_message(self._log_user['member'])
+        for channel in self._client.private_channels:
+            if len(channel.recipients) == 1 and channel.recipients[0].id == self._log_user['member'].id:
+                log_channel = channel
+
+        async for msg in self._client.logs_from(log_channel, limit=1):
+            return msg
+
+    async def log(self, msg_to_log, first_log=False):
+        timestamp_msg = self._get_time() + msg_to_log
+        edit_last_msg = False
+
+        if first_log:
+            last_msg = await self._get_last_msg()
+            if last_msg and last_msg.author.id == self._client.user.id and last_msg.content.startswith('-'):
+                edit_last_msg = True
+            else:
+                timestamp_msg = '-' * 90 + '\n' + timestamp_msg
+
+        print(timestamp_msg)
+        if edit_last_msg:
+            await self._client.edit_message(last_msg, last_msg.content + '\r\n' + timestamp_msg)
+        elif 'member' in self._log_user:
+            await self._client.send_message(self._log_user['member'], timestamp_msg)
